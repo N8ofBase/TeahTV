2a86381c39686afd5d415fedf0aea9c84693d778
N8ofBase <n8ofbase@gmail.com>
2021-08-10 19:20:57 -0500

    Add files via upload

---
+++ TeahTV/TeahTV/src/main/java/com/prismcortex/TeahTV/TeahTvApplication.java
@@ -0,0 +1,13 @@
+package com.prismcortex.TeahTV;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeahTvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeahTvApplication.class, args);
+	}
+
+}
---
+++ TeahTV/TeahTV/src/test/java/com/prismcortex/TeahTV/TeahTvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prismcortex.TeahTV;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeahTvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
