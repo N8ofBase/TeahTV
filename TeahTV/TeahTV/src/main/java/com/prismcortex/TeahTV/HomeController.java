34bcb8392f4d3866348c74285158583e817a2f89
PCN8 <prismcortex@outlook.com>
2021-08-12 14:43:26 -0500

    got page to load

--- TeahTV/TeahTV/src/main/java/com/prismcortex/TeahTV/HomeController.java
+++ TeahTV/TeahTV/src/main/java/com/prismcortex/TeahTV/HomeController.java
@@ -6,12 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("teahtv")
 public class HomeController {
 
-    @GetMapping("")
+    @RequestMapping("teahtv")
     public String homePage (Model model) {
-        model.addAttribute()
+        model.addAttribute("title", "Teah-TV");
         return "teahtv";
     }
 
